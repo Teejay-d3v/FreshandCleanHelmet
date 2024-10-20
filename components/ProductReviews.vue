2b85730e8aee6abb81798b4beaa5867446554a78
Teejay-d3v <lozadatimothyjohn@gmail.com>
2024-10-20 18:00:24 +0800

    ups

--- components/ProductReviews.vue
+++ components/ProductReviews.vue
@@ -12,66 +12,134 @@
           <div class="splide__track">
             <ul class="splide__list">
               <li class="splide__slide">
-                <div class="p-6 border-2 rounded-lg bg-white transform transition duration-500">
+                <div class="p-6 border-2 rounded-lg bg-white shadow-lg transform transition duration-500">
                   <div class="h-full text-center">
-                    <p class="leading-relaxed text-gray-700 italic">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolorem architecto provident modi aut sunt sint qui quo quae alias?"</p>
+                    <p class="leading-relaxed text-gray-700 italic">"Nagtry ako ng ibang helmet cleaning machine 2 months ago and sobrang daming issue, from poor cleaning performance to flimsy build. Pero nung ni recommend sya ng friend kong Mechanical engr. at nasubukan namin, I can tell na matibay at efficient. Plus, hassle-free pa ang operations dahil sa smart features. Super worth it compared sa dati!
+                      "</p>
                     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
                     <div class="flex items-center justify-center">
                       <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full border-4 border-indigo-500 shadow-lg" src="https://dummyimage.com/302x302">
                       <div class="ml-4 text-left">
+                        <h2 class="text-gray-900 font-bold title-font tracking-wider text-lg">d*****o
+                        </h2>
+                        <p class="text-gray-500">Business Woman</p>
+                      </div>
+
+                      <div class="ml-4 text-left">
                         <div class="flex mb-1">
+                          <i class="mt-2">Quality</i>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
-                          <span class="text-gray-300 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
                         </div>
-                        <h2 class="text-gray-900 font-bold title-font tracking-wider text-lg">Alex Morgan</h2>
-                        <p class="text-gray-500">Senior Product Designer</p>
+                        <div class="flex mb-1">
+                          <i class="mt-2">Appearance</i>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                        </div>
+                        <div class="flex mb-1">
+                          <i class="mt-2">Suitability</i>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-slate-300 text-2xl">★</span>
+                        </div>
+                       
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
+
               <li class="splide__slide">
                 <div class="p-6 border-2 rounded-lg bg-white shadow-lg transform transition duration-500">
                   <div class="h-full text-center">
-                    <p class="leading-relaxed text-gray-700 italic">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Est architecto laudantium recusandae voluptatem, fugiat, optio dolores voluptate asperiores soluta quidem totam molestiae odio explicabo numquam minus reprehenderit deserunt."</p>
+                    <p class="leading-relaxed text-gray-700 italic">"Ang ganda ng partnership namin with HelmetPro. Maayos yung machine, at solid yung support nila mula sa simula, hands-on sa setup at laging andyan kapag may concerns. Highly recommend sa mga naghahanap ng maaasahang business partner."</p>
                     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
                     <div class="flex items-center justify-center">
                       <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full border-4 border-indigo-500 shadow-lg" src="https://dummyimage.com/302x302">
                       <div class="ml-4 text-left">
+                        <h2 class="text-gray-900 font-bold title-font tracking-wider text-lg">s*****s</h2>
+                        <p class="text-gray-500">BPO Employee</p>
+                      </div>
+
+                      <div class="ml-4 text-left">
                         <div class="flex mb-1">
+                          <i class="mt-2">Quality</i>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                         </div>
-                        <h2 class="text-gray-900 font-bold title-font tracking-wider text-lg">Sophie Turner</h2>
-                        <p class="text-gray-500">Graphic Designer</p>
+                        <div class="flex mb-1">
+                          <i class="mt-2">Appearance</i>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-slate-300 text-2xl">★</span>
+                        </div>
+                        <div class="flex mb-1">
+                          <i class="mt-2">Suitability</i>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                        </div>
+                       
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
+
               <li class="splide__slide">
                 <div class="p-6 border-2 rounded-lg bg-white shadow-lg transform transition duration-500">
                   <div class="h-full text-center">
-                    <p class="leading-relaxed text-gray-700 italic">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolorem architecto provident modi aut sunt sint qui quo quae alias?"</p>
+                    <p class="leading-relaxed text-gray-700 italic">"Ang ganda ng partnership namin with HelmetPro. Maayos yung machine, at solid yung support nila mula sa simula—hands-on sa setup at laging andyan kapag may kailangan. Pinaka nagustuhan ko sa kanila ay may ‘Business Support Manager’ na naka-assign sa amin, lalo na sa mga tulad namin na walang masyadong experience sa negosyo, kaya malaking tulong ang guidance at assistance. Highly recommend sa mga naghahanap ng maaasahang business partner"</p>
                     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
                     <div class="flex items-center justify-center">
                       <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full border-4 border-indigo-500 shadow-lg" src="https://dummyimage.com/302x302">
                       <div class="ml-4 text-left">
+                        <h2 class="text-gray-900 font-bold title-font tracking-wider text-lg">m*****a
+                        </h2>
+                        <p class="text-gray-500">Business Partner</p>
+                      </div>
+
+                      <div class="ml-4 text-left">
                         <div class="flex mb-1">
+                          <i class="mt-2">Quality</i>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
                           <span class="text-yellow-500 text-2xl">★</span>
-                          <span class="text-gray-300 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
                         </div>
-                        <h2 class="text-gray-900 font-bold title-font tracking-wider text-lg">Michael Tan</h2>
-                        <p class="text-gray-500">Marketing Specialist</p>
+                        <div class="flex mb-1">
+                          <i class="mt-2">Appearance</i>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-slate-300 text-2xl">★</span>
+                        </div>
+                        <div class="flex mb-1">
+                          <i class="mt-2">Suitability</i>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                          <span class="text-yellow-500 text-2xl">★</span>
+                        </div>
+                       
                       </div>
                     </div>
                   </div>
