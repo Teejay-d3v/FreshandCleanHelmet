e936b0a4e70614df2bd21206f1198b9d2a32224e
Teejay-d3v <lozadatimothyjohn@gmail.com>
2024-10-22 03:54:38 +0800

    ups

--- components/Chatbot.vue
+++ components/Chatbot.vue
@@ -1,51 +1,36 @@
 <template>
-  <section class="h-[100px] flex items-center fixed bottom-6 right-6 justify-center z-50">
+  <section class="lg:h-[100px] flex items-center fixed bottom-6 right-6 justify-center z-50">
     <div class="relative flex flex-col items-center">
-      <button
-        @click="toggleButtons"
-        class=" z-10"
-      >
-        <img src="/public/images/chatlogo.png" class="h-[130px] chat-logo" :class="{ 'rotate': isRotating }">
+      <button @click="toggleButtons" class="z-10">
+        <img src="/public/images/chatlogo.png" class="lg:h-[130px] chat-logo" :class="{ 'rotate': isRotating }" />
       </button>
 
       <div v-if="showButtons" class="absolute flex flex-col items-center">
         <button
           ref="button1"
           class="circle-button bg-blue-500 text-white animate-show"
-          style="top: 0; left: 0;" 
+          style="top: 0; left: 0;"
         >
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLScPH78it2IObDmevPColeGFe16743x8IzgvmQNGKGxYfX2wDQ/viewform?usp=send_form"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Book Now
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLScPH78it2IObDmevPColeGFe16743x8IzgvmQNGKGxYfX2wDQ/viewform?usp=send_form" target="_blank" rel="noopener noreferrer">
+            Book Now
           </a>
         </button>
         <button
           ref="button2"
           class="circle-button bg-blue-500 text-white animate-show"
-          style="top: 0; left: 0;" 
+          style="top: 0; left: 0;"
         >
-        <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSc_isim53g1u6-pYQRLzhk75UUQjFSYdkI9_wYUrgZCABmH8A/viewform"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Inquire Now
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSc_isim53g1u6-pYQRLzhk75UUQjFSYdkI9_wYUrgZCABmH8A/viewform" target="_blank" rel="noopener noreferrer">
+            Inquire Now
           </a>
         </button>
         <button
           ref="button3"
           class="circle-button bg-blue-500 text-white animate-show z-10"
-          style="top: 0; left: 0;"  
+          style="top: 0; left: 0;"
         >
-        <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdagJhiw1LHysZAHzuqV16C6vQPvLy9dHs0o3bUxza1HqvV_Q/viewform"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Help Support
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdagJhiw1LHysZAHzuqV16C6vQPvLy9dHs0o3bUxza1HqvV_Q/viewform" target="_blank" rel="noopener noreferrer">
+            Help Support
           </a>
         </button>
       </div>
@@ -61,7 +46,7 @@ const isRotating = ref(false);
 
 const toggleButtons = () => {
   showButtons.value = !showButtons.value;
-  isRotating.value = !isRotating.value; // Toggle the rotation
+  isRotating.value = !isRotating.value;
 
   nextTick(() => {
     document.querySelectorAll('.animate-show').forEach((button, index) => {
@@ -75,36 +60,47 @@ const toggleButtons = () => {
 
 <style scoped>
 .chat-logo {
-  transition: transform 0.6s ease; /* Smooth transition for the rotation */
+  transition: transform 0.6s ease;
 }
 
 .rotate {
-  transform: rotate(360deg); /* Rotate the image 360 degrees */
+  transform: rotate(360deg);
 }
 
 .circle-button {
-  width: 70px; 
-  height: 70px; 
-  border-radius: 50%; 
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute; 
-  opacity: 0; 
-  animation-duration: 0.6s; 
-  animation-fill-mode: forwards; 
+  position: absolute;
+  opacity: 0;
+  animation-duration: 0.6s;
+  animation-fill-mode: forwards;
   transform-origin: center center;
 }
 
-/* Keyframes for move and circular animation around the button to the left */
+/* Responsive adjustments */
+@media (max-width: 768px) {
+  .circle-button {
+    width: 60px;
+    height: 60px;
+  }
+
+  .chat-logo {
+    height: 70px; /* Smaller logo on smaller screens */
+  }
+}
+
 @keyframes animateIn1 {
   0% {
     opacity: 0;
-    transform: translate(0, 0) scale(0.5); /* Start from the parent button */
+    transform: translate(0, 0) scale(0.5);
   }
   100% {
     opacity: 1;
-    transform: translate(-150px, 20px) scale(1); /* Move left */
+    transform: translate(-150px, 20px) scale(1);
   }
 }
 
@@ -115,7 +111,7 @@ const toggleButtons = () => {
   }
   100% {
     opacity: 1;
-    transform: translate(-110px, -60px) scale(1); /* Move diagonally up-left */
+    transform: translate(-110px, -60px) scale(1);
   }
 }
 
@@ -126,7 +122,7 @@ const toggleButtons = () => {
   }
   100% {
     opacity: 1;
-    transform: translate(-30px, -100px) scale(1); /* Move straight up */
+    transform: translate(-30px, -100px) scale(1);
   }
 }
 
@@ -137,7 +133,7 @@ const toggleButtons = () => {
   }
   100% {
     opacity: 0;
-    transform: translate(0, 0) scale(0.5); /* Return to the original position */
+    transform: translate(0, 0) scale(0.5);
   }
 }
 
@@ -154,7 +150,7 @@ const toggleButtons = () => {
 }
 
 .animate-out {
-  animation-name: animateOut; 
+  animation-name: animateOut;
 }
 
 .animate-out:nth-child(1) {
